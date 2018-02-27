2707c836668b766c7f60b1caf6b2c8784c43cc44
Akshay Subramaniam <6964110+akshaysubr@users.noreply.github.com>
2018-02-26 16:31:04 -0800

    Added the TEResNet model

--- model/model.py
+++ model/model.py
@@ -5,12 +5,12 @@ def generator(gen_inputs, gen_output_channels, reuse=False, FLAGS=None):
         raise  ValueError('No FLAGS is provided for generator')
 
     # The Bx residual blocks
-    def residual_block(inputs, output_channel, stride, scope):
+    def residual_block(inputs, output_channels, stride, scope):
         with tf.variable_scope(scope):
-            net = conv3d(inputs, 3, output_channel, stride, use_bias=False, scope='conv_1')
+            net = conv3d(inputs, 3, output_channels, stride, use_bias=False, scope='conv_1')
             net = batchnorm(net, FLAGS.is_training)
             net = prelu_tf(net)
-            net = conv3d(net, 3, output_channel, stride, use_bias=False, scope='conv_2')
+            net = conv3d(net, 3, output_channels, stride, use_bias=False, scope='conv_2')
             net = batchnorm(net, FLAGS.is_training)
             net = net + inputs
         return net
@@ -48,3 +48,54 @@ def generator(gen_inputs, gen_output_channels, reuse=False, FLAGS=None):
             net = conv3d(net, 9, gen_output_channels, 1, scope='conv')
 
     return net
+
+
+class TEResNet(object):
+
+    def __init__(self, inputs, targets, FLAGS):
+
+        # Build the generator part
+        with tf.variable_scope('generator'):
+            self.output_channels = targets.get_shape().as_list()[-1]
+            self.gen_output = generator(inputs, self.output_channels, reuse=False, FLAGS=FLAGS)
+            # self.gen_output.set_shape([FLAGS.batch_size, FLAGS.input_size * 4, FLAGS.input_size * 4, FLAGS.input_size * 4, 4])
+
+
+        # Calculating the generator loss
+        with tf.variable_scope('generator_loss'):
+
+            # Content loss
+            with tf.variable_scope('content_loss'):
+                # Compute the euclidean distance between the two features
+                self.content_loss = tf.reduce_mean( tf.square(self.gen_output - targets) )
+
+            self.gen_loss = self.content_loss
+
+        # Define the learning rate and global step
+        with tf.variable_scope('get_learning_rate_and_global_step'):
+
+            self.global_step = tf.contrib.framework.get_or_create_global_step()
+            self.learning_rate = tf.train.exponential_decay(FLAGS.learning_rate, self.global_step, FLAGS.decay_step, FLAGS.decay_rate,
+                                                       staircase=FLAGS.stair)
+            self.incr_global_step = tf.assign(self.global_step, self.global_step + 1)
+
+        with tf.variable_scope('generator_train'):
+
+            # Need to wait discriminator to perform train step
+            with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
+
+                gen_tvars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='generator')
+                gen_optimizer = tf.train.AdamOptimizer(self.learning_rate, beta1=FLAGS.beta)
+                self.gen_train = gen_optimizer.minimize( self.gen_loss )
+
+        exp_averager = tf.train.ExponentialMovingAverage(decay=0.99)
+        self.update_loss = exp_averager.apply([self.content_loss])
+
+
+    def optimize(self, session):
+
+        results = session.run( (self.update_loss, self.incr_global_step, self.gen_train) )
+
+
+
+
--- model/ops.py
+++ model/ops.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-import tensorFlor.contrib.slim as slim
+import tensorflow.contrib.slim as slim
 
 def periodic_padding(inpt, pad):
     L = inpt[:,:pad[0][0],:,:,:]
@@ -34,7 +34,7 @@ def conv3d_withPeriodicPadding(inpt, filtr, strides, name=None):
     
     return output
 
-def conv3d(inpt, f, output_channels, s, use_bias=False, scope, name=None):
+def conv3d(inpt, f, output_channels, s, use_bias=False, scope='conv', name=None):
     inpt_shape = inpt.get_shape().as_list()
     with tf.variable_scope(scope):
         filtr = tf.get_variable(initializer=tf.contrib.layers.xavier_initializer(),
@@ -67,7 +67,37 @@ def batchnorm(inputs, is_training):
                            scale=False,fused=True,is_training=is_training)
 
 
-                                   
+def phaseShift(inputs, shape_1, shape_2):
+    # Tackle the condition when the batch is None
+    X = tf.reshape(inputs, shape_1)
+    X = tf.transpose(X, [0, 1, 4, 2, 5, 3, 6])
+
+    return tf.reshape(X, shape_2)
+
+
+# The implementation of PixelShuffler
+def pixelShuffler(inputs, scale=2):
+    size = tf.shape(inputs)
+    batch_size = size[0]
+    d = size[1]
+    h = size[2]
+    w = size[3]
+    c = size[4]
+
+    # Get the target channel size
+    channel_target = c // (scale * scale * scale)
+    channel_factor = c // channel_target
+
+    shape_1 = [batch_size, d, h, w, scale, scale, scale]
+    shape_2 = [batch_size, d * scale, h * scale, w * scale, 1]
+
+    # Reshape and transpose for periodic shuffling for each channel
+    input_split = tf.split(inputs, channel_target, axis=4)
+    output = tf.concat([phaseShift(x, shape_1, shape_2) for x in input_split], axis=4)
+
+    return output
+
+
 if __name__ == "__main__":
     import numpy as np
     
@@ -92,3 +122,17 @@ if __name__ == "__main__":
        
     print(XConv.shape)
     print(XConv[0,:,:,:,0])
+
+    Xphase = tf.placeholder(tf.float32, shape=(1, 4, 4, 4, 4))
+    shape_1 = [1, 4, 4, 4, 2, 2, 1]
+    shape_2 = [1, 8, 8, 4, 1]
+    X_ups = phaseShift(Xphase, shape_1, shape_2)
+
+    with tf.Session() as sess:
+        xphase = np.zeros((1,4,4,4,4))
+        for i in range(4):
+            xphase[:,:,:,:,i] = i
+        xups = sess.run( X_ups, feed_dict={ Xphase: xphase } )
+
+    print(xups[0,:,:,0,0])
+    print(xups.shape)
