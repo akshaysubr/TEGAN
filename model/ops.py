ac05e8f85108a375abd580194e0b2a8a4fec4fe2
Raunak Borker <rborker@rice12.stanford.edu>
2018-02-26 17:46:09 -0800

    ops.py updated to add print_configuration

--- model/ops.py
+++ model/ops.py
@@ -67,6 +67,24 @@ def batchnorm(inputs, is_training):
                            scale=False,fused=True,is_training=is_training)
 
 
+def print_configuration_op(FLAGS):
+    print('[Configurations]:')
+    a = FLAGS.mode
+    #pdb.set_trace()
+    for name, value in FLAGS.__flags.items():
+        if type(value) == float:
+            print('\t%s: %f'%(name, value))
+        elif type(value) == int:
+            print('\t%s: %d'%(name, value))
+        elif type(value) == str:
+            print('\t%s: %s'%(name, value))
+        elif type(value) == bool:
+            print('\t%s: %s'%(name, value))
+        else:
+            print('\t%s: %s' % (name, value))
+
+    print('End of configuration')
+
                                    
 if __name__ == "__main__":
     import numpy as np
