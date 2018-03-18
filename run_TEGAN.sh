76fa55106399df34314d379bfe9be846249aa369
Raunak Borker <1243623+raunakborker@users.noreply.github.com>
2018-03-17 19:05:55 -0700

    updating run scripts to include decay rate and changed formatting

--- run_TEGAN.sh
+++ run_TEGAN.sh
@@ -15,23 +15,46 @@ cd /home/${USER}/Codes/TEGAN
 module purge
 module load cuda/9.0 cudnn/7.0.3
 export LD_LIBRARY_PATH=/usr/local/cuda-9.0/extras/CUPTI/lib64:${LD_LIBRARY_PATH}
-source ~/tensorflow-gpu/bin/activate
+source ~/tensorFlowGpu/bin/activate
 
 TASK="TEGAN"
 LEARNING_RATE=0.0001
+DECAY_STEP=600
+DECAY_RATE=0.5
 RESBLOCKS=12
-GENFREQ=3
-GENSTART=200
-ADVERSARIAL_RATIO=0.001
+GENFREQ=2
+GENSTART=300
+ADVERSARIAL_RATIO=0.01
 ENS=0.2
 CON=0.5
 PHY=0.125
 
 SUFFIX_RESNET="_RB${RESBLOCKS}_LR${LEARNING_RATE}_ENS${ENS}_CON${CON}_PHY${PHY}"
-SUFFIX="_RB${RESBLOCKS}_LR${LEARNING_RATE}_ENS${ENS}_CON${CON}_PHY${PHY}_GS${GENSTART}_GF${GENFREQ}_AR${ADVERSARIAL_RATIO}"
+SUFFIX="_RB${RESBLOCKS}_LR${LEARNING_RATE}_ENS${ENS}_CON${CON}_PHY${PHY}_GS${GENSTART}_GF${GENFREQ}_AR${ADVERSARIAL_RATIO}_DR${DECAY_RATE}_DS${DECAY_STEP}"
 
-CMD="python main.py --task ${TASK} --checkpoint /farmshare/user_data/${USER}/TEGAN/TEResNet/output${SUFFIX_RESNET}/model-9200 --pre_trained_generator True --num_resblock ${RESBLOCKS} --learning_rate ${LEARNING_RATE} --gen_start ${GENSTART} --gen_freq ${GENFREQ} --adversarial_ratio ${ADVERSARIAL_RATIO} --lambda_ens ${ENS} --lambda_con ${CON} --lambda_phy ${PHY} --train_dir /farmshare/user_data/${USER}/TEGAN/Data/train --dev_dir /farmshare/user_data/${USER}/TEGAN/Data/dev --max_iter 10000 --max_epoch 50 --save_freq 100 --summary_freq 10 --output_dir /farmshare/user_data/${USER}/TEGAN/${TASK}/output${SUFFIX} --summary_dir /farmshare/user_data/${USER}/TEGAN/${TASK}/summary${SUFFIX} --log_file /farmshare/user_data/${USER}/TEGAN/${TASK}/log${SUFFIX}.dat"
+CMD="python main.py \
+--task ${TASK} \
+--num_resblock ${RESBLOCKS} \
+--learning_rate ${LEARNING_RATE} \
+--gen_start ${GENSTART} \
+--gen_freq ${GENFREQ} \
+--adversarial_ratio ${ADVERSARIAL_RATIO} \
+--lambda_ens ${ENS} \
+--lambda_con ${CON} \
+--lambda_phy ${PHY} \
+--max_iter 10000 \
+--max_epoch 50 \
+--save_freq 100 \
+--summary_freq 10 \
+--train_dir /farmshare/user_data/${USER}/TEGAN/Data/train \
+--dev_dir /farmshare/user_data/${USER}/TEGAN/Data/test \
+--output_dir /farmshare/user_data/${USER}/TEGAN/${TASK}/output${SUFFIX} \
+--summary_dir /farmshare/user_data/${USER}/TEGAN/${TASK}/summary${SUFFIX} \
+--log_file /farmshare/user_data/${USER}/TEGAN/${TASK}/log${SUFFIX}.dat \
+--checkpoint /farmshare/user_data/${USER}/TEGAN/TEResNet/output${SUFFIX_RESNET}/model-9200 \
+--pre_trained_generator True "
+
 
 echo ${CMD}
 
-${CMD}
+${CMD} >& log${SUFFIX}.out
--- run_TEResNet.sh
+++ run_TEResNet.sh
@@ -9,23 +9,42 @@
 #SBATCH --qos=normal
 #SBATCH --gres=gpu:1
 
+USER=rborker
 cd /home/${USER}/Codes/TEGAN
 
 # Setup the environment
 module purge
 module load cuda/9.0 cudnn/7.0.3
 export LD_LIBRARY_PATH=/usr/local/cuda-9.0/extras/CUPTI/lib64:${LD_LIBRARY_PATH}
-source ~/tensorflow-gpu/bin/activate
+source ~/tensorFlowGpu/bin/activate
 
 TASK="TEResNet"
 LEARNING_RATE=0.0001
 RESBLOCKS=12
 ENS=0.2
 CON=0.5
-PHY=0.0
+PHY=0.125
 SUFFIX="_RB${RESBLOCKS}_LR${LEARNING_RATE}_ENS${ENS}_CON${CON}_PHY${PHY}"
 
-CMD="python main.py --task ${TASK} --num_resblock ${RESBLOCKS} --learning_rate ${LEARNING_RATE} --lambda_ens ${ENS} --lambda_con ${CON} --lambda_phy ${PHY} --train_dir /farmshare/user_data/${USER}/TEGAN/Data/train --dev_dir /farmshare/user_data/${USER}/TEGAN/Data/dev --max_iter 10000 --max_epoch 50 --save_freq 100 --summary_freq 10 --output_dir /farmshare/user_data/${USER}/TEGAN/${TASK}/output${SUFFIX} --summary_dir /farmshare/user_data/${USER}/TEGAN/${TASK}/summary${SUFFIX} --log_file /farmshare/user_data/${USER}/TEGAN/${TASK}/log${SUFFIX}.dat"
+CMD="python main.py \
+--task ${TASK} \
+--num_resblock ${RESBLOCKS} \
+--learning_rate ${LEARNING_RATE} \
+--lambda_ens ${ENS} \
+--lambda_con ${CON} \
+--lambda_phy ${PHY} \
+--max_iter 10000 \
+--max_epoch 50 \
+--save_freq 100 \
+--summary_freq 10 \
+--train_dir /farmshare/user_data/${USER}/TEGAN/Data/train_small \
+--dev_dir /farmshare/user_data/${USER}/TEGAN/Data/test_small \
+--output_dir /farmshare/user_data/${USER}/TEGAN/${TASK}/output_small${SUFFIX} \
+--summary_dir /farmshare/user_data/${USER}/TEGAN/${TASK}/summary_small${SUFFIX} \
+--log_file /farmshare/user_data/${USER}/TEGAN/${TASK}/log${SUFFIX}.dat \
+--checkpoint /farmshare/user_data/${USER}/TEGAN/TEResNet/output${SUFFIX_RESNET}/model-9200 \
+--pre_trained_model True "
+
 
 echo ${CMD}
 
